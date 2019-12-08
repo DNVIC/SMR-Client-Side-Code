df6f477afba0a9df4310f028370dbf17d4d3dd71
BuildTools <unconfigured@null.spigotmc.org>
2019-12-08 12:24:54 -0500

    Version 1.0
    Added stuff I made in Visual Studio

---
+++ ClientSideSMR/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using ClientSideSMR;
+
+namespace ClientSideSMR
+{
+    
+    class Program
+    {
+        private static string _user;
+        private static string _oauth;
+        private static string _channel;
+
+        
+        public static void assignStrings()
+        {
+            Console.Write("Insert given username");
+            string user = Console.ReadLine();
+            Console.Write("Insert given password");
+            string pass = Console.ReadLine();
+            Console.Write("Insert given channel");
+            string chan = Console.ReadLine();
+
+
+            _user = user;
+            _oauth = pass;
+            _channel = chan;
+        }
+
+        static async Task Main(string[] args)
+        {
+            assignStrings();
+            await ExecuteClient();
+        }
+        static async Task ExecuteClient()
+        {
+            // Base socket code taken from https://geeksforgeeks.org/socket-programming-in-c-sharp/
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipaddr = ipHost.AddressList[2];
+                IPEndPoint localEndPoint = new IPEndPoint(ipaddr, 16834);
+                Console.WriteLine(ipaddr.ToString());
+                Socket sender = new Socket(ipaddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    //Connect socket to endpoint
+                    sender.Connect(localEndPoint);
+                    byte[] ByteBuffer = new byte[1024];
+
+
+
+                    //Print information that means we are good
+                    Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
+
+                    /*
+                    //Creating sent message
+                    byte[] messageSent = Encoding.ASCII.GetBytes("starttimer\r\n");
+                    //Sending
+                    int byteSent = sender.Send(messageSent);
+                    messageSent = Encoding.ASCII.GetBytes("split\r\n");
+                    int byte2Sent = sender.Send(messageSent);
+                    */
+
+                    await SplitLevelChecker(sender, ByteBuffer);
+                    //sender.Shutdown(SocketShutdown.Both);
+                    //sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected Exception : {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void SendCommand(Socket s, string Command)
+        {
+            byte[] spinx = Encoding.ASCII.GetBytes(Command + "\r\n");
+            s.Send(spinx);   
+        }
+
+        static string ReceiveCommand(Socket s, byte[] Buffer)
+        {
+            
+            int recv = s.Receive(Buffer);
+
+            
+
+
+            return Encoding.ASCII.GetString(Buffer, 0, recv);
+        }
+
+        static string SendAndReceiveCommand(Socket s, string Command, byte[] Buffer)
+        {
+            byte[] spinx = Encoding.ASCII.GetBytes(Command + "\r\n");
+            s.Send(spinx);
+            int recv = s.Receive(Buffer);
+            return Encoding.ASCII.GetString(Buffer, 0, recv);
+        }
+
+
+        private static async Task SplitLevelChecker(Socket sender, byte[] ByteBuffer )
+        {
+            Boolean bowl = true;
+            Console.Write("When DNVIC tells you to start, press Enter here. \nIt will automatically start your splits, so only worry about pressing enter and starting the game.");
+            Console.ReadLine();
+            SendCommand(sender, "starttimer");
+            string CurrentProgress = "";
+            IrcClient ircClient = new IrcClient("irc.chat.twitch.tv", 6667, _user, _oauth, _channel);
+
+            PingSender ping = new PingSender(ircClient);
+            ping.Start();
+            while (bowl)
+            {
+
+
+                string ReceivedCommand = SendAndReceiveCommand(sender, "getcurrentsplitname", ByteBuffer);
+                string SplitID = ReceivedCommand.Substring(0, 2);
+                Console.WriteLine(SplitID);
+
+                
+
+                if(SplitID != "!!")
+                {
+                    CurrentProgress = SplitID;
+                }
+
+                Console.WriteLine("Current Progress" + CurrentProgress);
+                ircClient.SendPublicChatMessage(CurrentProgress);
+
+                await Task.Delay(1500);
+            }
+        }
+        
+    }
+}
---
+++ ClientSideSMR/gayclient.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+//Partially taken from https://codereview.stackexchange.com/questions/142653/simple-irc-bot-in-c
+
+
+namespace ClientSideSMR
+{
+    public class gayclient
+    {
+        // server to connect to (edit at will)
+        private readonly string _server;
+        // server port (6667 by default)
+        private readonly int _port;
+        // user information defined in RFC 2812 (IRC: Client Protocol) is sent to the IRC server 
+        private readonly string _user;
+
+        // the bot's nickname
+        //private readonly string _nick;
+        // channel to join
+        private readonly string _channel;
+
+        private readonly string _password;
+
+        private readonly int _maxRetries;
+        public StreamWriter writer;
+
+
+        public gayclient(string server, int port, string user, string channel, string password, int maxRetries = 3)
+        {
+            _server = server;
+            _port = port;
+            _user = user;
+            _channel = channel;
+            _maxRetries = maxRetries;
+            _password = password;
+
+        }
+
+        public async Task Start()
+        {
+            var retry = false;
+            var retryCount = 0;
+
+
+            do
+            {
+                try
+                {
+                    using (var irc = new TcpClient(_server, _port))
+                    using (var stream = irc.GetStream())
+                    using (var reader = new StreamReader(stream))
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        writer.WriteLine("PASS " + _password);
+                        writer.WriteLine("NICK " + _user);
+                        writer.WriteLine("USER " + _user + " 8 * :" + _user);
+                        writer.Flush();
+
+                        while (true)
+                        {
+                            string inputLine;
+                            while ((inputLine = reader.ReadLine()) != null)
+                            {
+                                Console.WriteLine("<- " + inputLine);
+
+                                // split the lines sent from the server by spaces (seems to be the easiest way to parse them)
+                                string[] splitInput = inputLine.Split(new Char[] { ' ' });
+
+                                if (splitInput[0] == "PING")
+                                {
+                                    string PongReply = splitInput[1];
+                                    //Console.WriteLine("->PONG " + PongReply);
+                                    writer.WriteLine("PONG " + PongReply);
+                                    writer.Flush();
+                                    //continue;
+                                }
+
+                                switch (splitInput[1])
+                                {
+                                    case "001":
+                                        writer.WriteLine("JOIN #" + _channel);
+                                        writer.Flush();
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    // shows the exception, sleeps for a little while and then tries to establish a new connection to the IRC server
+                    Console.WriteLine(e.ToString());
+                    await Task.Delay(5000);
+                    retry = ++retryCount <= _maxRetries;
+                }
+            } while (retry);
+        }
+
+        public void SendIrcMessage(string message, StreamWriter writer)
+        {
+            try
+            {
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void SendPublicChatMessage(string message, StreamWriter writer)
+        {
+            try
+            {
+                SendIrcMessage(":" + _user + "!" + _user + "@" + _user + "tmi.twitch.tv PRIVMSG #ldlotbot :" + message, writer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
